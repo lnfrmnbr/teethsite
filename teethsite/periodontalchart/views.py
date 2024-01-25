68360c589d2705a12287be5fc4532853fcb71a65
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-01-25 23:33:39 +0300

    +кровоточивость

--- teethsite/periodontalchart/views.py
+++ teethsite/periodontalchart/views.py
@@ -4,15 +4,3 @@ def ind(request):
 
 def about(request):
     return render(request, 'periodontalchart/about.html')
-
-#def before(request):
- #   return render(request, 'periodontalchart/index.html', {
-  #      'image_src': 'img/1.PNG',
-   #     'message': 'Hii! GeeksforGeeks people'
-   # })
-
-#def after(request):
- #   return render(request, 'periodontalchart/index.html', {
-   #     'image_src': 'img/2.PNG',
-    #    'message': 'Bye! GeeksforGeeks people'
-   # })
