c192291c1f93f500709e63468fc0905a3b76de33
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-02-14 15:17:55 +0300

    kr.js и otl.js

---
+++ teethsite/periodontalchart/static/periodontalchart/js/kr.js
@@ -0,0 +1,287 @@
+function kr18_1() {
+           var el = document.getElementById("kr18_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr18_2() {
+           var el = document.getElementById("kr18_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr18_3() {
+           var el = document.getElementById("kr18_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr17_1() {
+           var el = document.getElementById("kr17_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr17_2() {
+           var el = document.getElementById("kr17_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr17_3() {
+           var el = document.getElementById("kr17_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr16_1() {
+           var el = document.getElementById("kr16_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr16_2() {
+           var el = document.getElementById("kr16_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr16_3() {
+           var el = document.getElementById("kr16_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr15_1() {
+           var el = document.getElementById("kr15_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr15_2() {
+           var el = document.getElementById("kr15_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr15_3() {
+           var el = document.getElementById("kr15_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr14_1() {
+           var el = document.getElementById("kr14_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr14_2() {
+           var el = document.getElementById("kr14_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr14_3() {
+           var el = document.getElementById("kr14_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr13_1() {
+           var el = document.getElementById("kr13_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr13_2() {
+           var el = document.getElementById("kr13_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr13_3() {
+           var el = document.getElementById("kr13_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr12_1() {
+           var el = document.getElementById("kr12_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr12_2() {
+           var el = document.getElementById("kr12_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr12_3() {
+           var el = document.getElementById("kr12_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr11_1() {
+           var el = document.getElementById("kr11_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr11_2() {
+           var el = document.getElementById("kr11_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function kr11_3() {
+           var el = document.getElementById("kr11_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'red';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
---
+++ teethsite/periodontalchart/static/periodontalchart/js/otl.js
@@ -0,0 +1,287 @@
+function otl18_1() {
+           var el = document.getElementById("otl18_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl18_2() {
+           var el = document.getElementById("otl18_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl18_3() {
+           var el = document.getElementById("otl18_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl17_1() {
+           var el = document.getElementById("otl17_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl17_2() {
+           var el = document.getElementById("otl17_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl17_3() {
+           var el = document.getElementById("otl17_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl16_1() {
+           var el = document.getElementById("otl16_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl16_2() {
+           var el = document.getElementById("otl16_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl16_3() {
+           var el = document.getElementById("otl16_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl14_1() {
+           var el = document.getElementById("otl14_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl14_2() {
+           var el = document.getElementById("otl14_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl14_3() {
+           var el = document.getElementById("otl14_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl13_1() {
+           var el = document.getElementById("otl13_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl13_2() {
+           var el = document.getElementById("otl13_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl13_3() {
+           var el = document.getElementById("otl13_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl12_1() {
+           var el = document.getElementById("otl12_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl12_2() {
+           var el = document.getElementById("otl12_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl12_3() {
+           var el = document.getElementById("otl12_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl11_1() {
+           var el = document.getElementById("otl11_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl11_2() {
+           var el = document.getElementById("otl11_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl11_3() {
+           var el = document.getElementById("otl11_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl15_1() {
+           var el = document.getElementById("otl15_1");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl15_2() {
+           var el = document.getElementById("otl15_2");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
+
+function otl15_3() {
+           var el = document.getElementById("otl15_3");
+           if (el.name == 0){
+              el.style.backgroundColor = 'blue';
+              el.name = 1;
+           }
+           else {
+              el.style.backgroundColor = '#fff0f5';
+              el.name = 0;
+           }
+}
