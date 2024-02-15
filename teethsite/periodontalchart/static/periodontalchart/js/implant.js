70338dadb2b3df58fe3494767b02644a1ea5bee6
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-02-15 17:03:16 +0300

    +test imlant pngs

---
+++ teethsite/periodontalchart/static/periodontalchart/js/implant.js
@@ -0,0 +1,90 @@
+function implant18() {
+           var el = document.getElementById("t18");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t18i.PNG";
+              el.name = 1;
+              document.getElementById('furk18').hidden = true;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t18.PNG";
+              el.name = 0;
+              document.getElementById('furk18').hidden = false;
+           }
+}
+
+function implant17() {
+           var el = document.getElementById("t17");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t17i.PNG";
+              el.name = 1;
+              document.getElementById('furk17').hidden = true;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t17.PNG";
+              el.name = 0;
+              document.getElementById('furk17').hidden = false;
+           }
+}
+
+function implant16() {
+           var el = document.getElementById("t16");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t16i.PNG";
+              el.name = 1;
+              document.getElementById('furk16').hidden = true;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t16.PNG";
+              el.name = 0;
+              document.getElementById('furk16').hidden = false;
+           }
+}
+
+function implant15() {
+           var el = document.getElementById("t15");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t15i.PNG";
+              el.name = 1;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t15.PNG";
+              el.name = 0;
+           }
+}
+
+function implant14() {
+           var el = document.getElementById("t14");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t14i.PNG";
+              el.name = 1;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t14.PNG";
+              el.name = 0;
+           }
+}
+
+function implant13() {
+           var el = document.getElementById("t13");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t13i.PNG";
+              el.name = 1;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t13.PNG";
+              el.name = 0;
+           }
+}
+
+function implant12() {
+           var el = document.getElementById("t12");
+           if (el.name == 0){
+              el.src = "static/periodontalchart/img/t12i.PNG";
+              el.name = 1;
+           }
+           else {
+              el.src = "static/periodontalchart/img/t15.PNG";
+              el.name = 0;
+           }
+}
+
