15ff4682560c4daf1bdfbf7ba36dbfeedd552bc4
Канюк Виктория <vikanyuk@ya.ru>
2024-02-26 11:18:08 +0300

    попытки сделать фуркацию

--- teethsite/periodontalchart/static/periodontalchart/js/otl.js
+++ teethsite/periodontalchart/static/periodontalchart/js/otl.js
@@ -1,3 +1,60 @@
+function otlozh_dop(id){
+     var el = document.getElementById(id);
+     el.style.backgroundColor = 'blue';
+     el.name = 1;
+}
+
+function otlozh1() {
+           otlozh_dop("otl18_1")
+           otlozh_dop("otl18_2");
+           otlozh_dop("otl18_3");
+           otlozh_dop("otl17_1");
+           otlozh_dop("otl17_2");
+           otlozh_dop("otl17_3");
+           otlozh_dop("otl16_1");
+           otlozh_dop("otl16_2");
+           otlozh_dop("otl16_3");
+           otlozh_dop("otl15_1");
+           otlozh_dop("otl15_2");
+           otlozh_dop("otl15_3");
+           otlozh_dop("otl14_1");
+           otlozh_dop("otl14_2");
+           otlozh_dop("otl14_3");
+           otlozh_dop("otl13_1");
+           otlozh_dop("otl13_2");
+           otlozh_dop("otl13_3");
+           otlozh_dop("otl12_1");
+           otlozh_dop("otl12_2");
+           otlozh_dop("otl12_3");
+           otlozh_dop("otl11_1");
+           otlozh_dop("otl11_2");
+           otlozh_dop("otl11_3");
+           otlozh_dop("otl21_1");
+           otlozh_dop("otl21_2");
+           otlozh_dop("otl21_3");
+           otlozh_dop("otl22_1");
+           otlozh_dop("otl22_2");
+           otlozh_dop("otl22_3");
+           otlozh_dop("otl23_1");
+           otlozh_dop("otl23_2");
+           otlozh_dop("otl23_3");
+           otlozh_dop("otl24_1");
+           otlozh_dop("otl24_2");
+           otlozh_dop("otl24_3");
+           otlozh_dop("otl25_1");
+           otlozh_dop("otl25_2");
+           otlozh_dop("otl25_3");
+           otlozh_dop("otl26_1");
+           otlozh_dop("otl26_2");
+           otlozh_dop("otl26_3");
+           otlozh_dop("otl27_1");
+           otlozh_dop("otl27_2");
+           otlozh_dop("otl27_3");
+           otlozh_dop("otl28_1");
+           otlozh_dop("otl28_2");
+           otlozh_dop("otl28_3");
+}
+
 function otl18_1() {
            var el = document.getElementById("otl18_1");
            if (el.name == 0){
