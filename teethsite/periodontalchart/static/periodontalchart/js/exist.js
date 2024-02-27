beececcd216aef96e74f1214fee229ad21aeeb1d
Канюк Виктория <vikanyuk@ya.ru>
2024-02-27 16:53:02 +0300

    exist одной функцией

--- teethsite/periodontalchart/static/periodontalchart/js/exist.js
+++ teethsite/periodontalchart/static/periodontalchart/js/exist.js
@@ -1,48 +1,39 @@
-function exist18() {
-           var el = document.getElementById("exist18");
+function create_ls_of_id(id){
+     var ls = ["podv??", "implant??", "kr??_1", "kr??_2", "kr??_3", "otl??_1", "otl??_2", "otl??_3", "desn??_1", "desn??_2", "desn??_3", "zond??_1", "zond??_2", "zond??_3", "fi??"];
+     for (var i =0; i< ls.length; i++){
+                    ls[i] = ls[i].replace("??", id);
+     }
+     return ls;
+}
+
+function exist(id_) {
+           var id = String(id_);
+           var el = document.getElementById("exist"+id);
+           var ls = create_ls_of_id(id);
            if (el.name == 0){
-              el.src = "static/periodontalchart/img/no.PNG";
               el.name = 1;
-              document.getElementById('podv18').hidden = true;
-              document.getElementById('implant18').hidden = true;
-              document.getElementById('furk18').hidden = true;
-              document.getElementById('kr18_1').hidden = true;
-              document.getElementById('kr18_2').hidden = true;
-              document.getElementById('kr18_3').hidden = true;
-              document.getElementById('otl18_1').hidden = true;
-              document.getElementById('otl18_2').hidden = true;
-              document.getElementById('otl18_3').hidden = true;
-              document.getElementById('desn18_1').hidden = true;
-              document.getElementById('desn18_2').hidden = true;
-              document.getElementById('desn18_3').hidden = true;
-              document.getElementById('zond18_1').hidden = true;
-              document.getElementById('zond18_2').hidden = true;
-              document.getElementById('zond18_3').hidden = true;
-              document.getElementById('fi18').hidden = true;
-              if (document.getElementById('t18').name == 1){
-                    document.getElementById('impl_kvadr18').hidden = true;
-                    document.getElementById('t18').name = 0;
-                    document.getElementById('t18').src = "static/periodontalchart/img/t18.PNG";
+              for (var i =0; i< ls.length; i++){
+                    document.getElementById(ls[i]).hidden = true;
               }
+              if (id_ == 18 || id_ == 17 || id_ == 16 || id_ == 28 || id_ == 27 || id_ == 26){
+                    document.getElementById("furk"+id).hidden = true;
+              }
+              if (document.getElementById('t'+id).name == 1){
+                    document.getElementById('impl_kvadr'+id).hidden = true;
+                    document.getElementById('t'+id).name = 0;
+                    document.getElementById('t'+id).src = "static/periodontalchart/img/t"+id+".PNG";
+              }
+              document.getElementById('no'+id).hidden = false;
            }
            else {
               el.src = "";
               el.name = 0;
-              document.getElementById('podv18').hidden = false;
-              document.getElementById('implant18').hidden = false;
-              document.getElementById('furk18').hidden = false;
-              document.getElementById('kr18_1').hidden = false;
-              document.getElementById('kr18_2').hidden = false;
-              document.getElementById('kr18_3').hidden = false;
-              document.getElementById('otl18_1').hidden = false;
-              document.getElementById('otl18_2').hidden = false;
-              document.getElementById('otl18_3').hidden = false;
-              document.getElementById('desn18_1').hidden = false;
-              document.getElementById('desn18_2').hidden = false;
-              document.getElementById('desn18_3').hidden = false;
-              document.getElementById('zond18_1').hidden = false;
-              document.getElementById('zond18_2').hidden = false;
-              document.getElementById('zond18_3').hidden = false;
-              document.getElementById('fi18').hidden = false;
+              for (var i =0; i< ls.length; i++){
+                    document.getElementById(ls[i]).hidden = false;
+              }
+              if (id_ == 18 || id_ == 17 || id_ == 16 || id_ == 28 || id_ == 27 || id_ == 26){
+                    document.getElementById("furk"+id).hidden = false;
+              }
+              document.getElementById('no'+id).hidden = true;
            }
-   }
+}
