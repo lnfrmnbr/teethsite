814b4e7955a31b9681a9fefee1cc7172e5775852
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-06-11 00:29:39 +0300

    кариес 2 ряд

--- teethsite/periodontalchart/static/periodontalchart/js/download.js
+++ teethsite/periodontalchart/static/periodontalchart/js/download.js
@@ -1,4 +1,5 @@
 function download() {
+    document.getElementById('download').disabled = 'true';
     const element1 = document.getElementById('top');
     const element2 = document.getElementById('div_tables1');
     const element3 = document.getElementById('div_tables2');
@@ -12,22 +13,23 @@ function download() {
         doc.addImage(imgTop, 18, 10, 172, 31);
         html2canvas(element2).then(function (canvas2) {
             const imgTable1 = canvas2.toDataURL();
-            doc.addImage(imgTable1, 10, 60, 190, 100);
+            doc.addImage(imgTable1, -20, 45, 250, 118);
 
             html2canvas(element3).then(function (canvas3) {
                 const imgTable2 = canvas3.toDataURL();
-                doc.addImage(imgTable2, 10, 90, 190, 100);
+                doc.addImage(imgTable2, -20, 190, 250, 90);
 
                 html2canvas(element4).then(function (canvas4) {
+                    doc.addPage();
                     const imgTable3 = canvas4.toDataURL();
-                    doc.addImage(imgTable3, 10, 120, 190, 100);
+                    doc.addImage(imgTable3, 10, 10, 190, 90);
 
                         html2canvas(element5).then(function (canvas5) {
-                            doc.addPage();
                             const imgTable4 = canvas5.toDataURL();
-                            doc.addImage(imgTable4, 10, 15, 190, 100);
+                            doc.addImage(imgTable4, 10, 90, 190, 121);
 
-                            doc.save('klinic_card.pdf');
+                            doc.save('clinical_chart.pdf');
+                            document.getElementById('download').disabled = 'false';
                         });
                 });
             });
