a9319855dccb8c6f407545c4903f233d31c12566
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-06-16 17:23:53 +0300

    exist ~ 2кариес

--- teethsite/periodontalchart/static/periodontalchart/js/exist.js
+++ teethsite/periodontalchart/static/periodontalchart/js/exist.js
@@ -83,24 +83,48 @@ function exist(id_) {
                     document.getElementById('t'+id+"kar32").name = 0;
                     document.getElementById('t'+id+"kar41").name = 0;
                     document.getElementById('t'+id+"kar42").name = 0;
+                    document.getElementById('t'+id+"2kar6").name = 0;
+                    document.getElementById('t'+id+"2kar5").name = 0;
+                    document.getElementById('t'+id+"2kar31").name = 0;
+                    document.getElementById('t'+id+"2kar32").name = 0;
+                    document.getElementById('t'+id+"2kar41").name = 0;
+                    document.getElementById('t'+id+"2kar42").name = 0;
                     document.getElementById('t'+id+"kar6").hidden = true;
                     document.getElementById('t'+id+"kar5").hidden = true;
                     document.getElementById('t'+id+"kar31").hidden = true;
                     document.getElementById('t'+id+"kar32").hidden = true;
                     document.getElementById('t'+id+"kar41").hidden = true;
                     document.getElementById('t'+id+"kar42").hidden = true;
+                    document.getElementById('t'+id+"2kar6").hidden = true;
+                    document.getElementById('t'+id+"2kar5").hidden = true;
+                    document.getElementById('t'+id+"2kar31").hidden = true;
+                    document.getElementById('t'+id+"2kar32").hidden = true;
+                    document.getElementById('t'+id+"2kar41").hidden = true;
+                    document.getElementById('t'+id+"2kar42").hidden = true;
                     document.getElementById("kar"+id+"_6").style.backgroundColor = '#f0f0f0';
                     document.getElementById("kar"+id+"_5").style.backgroundColor = '#f0f0f0';
                     document.getElementById("kar"+id+"_31").style.backgroundColor = '#f0f0f0';
                     document.getElementById("kar"+id+"_32").style.backgroundColor = '#f0f0f0';
                     document.getElementById("kar"+id+"_41").style.backgroundColor = '#f0f0f0';
                     document.getElementById("kar"+id+"_42").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_6").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_5").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_31").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_32").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_41").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_42").style.backgroundColor = '#f0f0f0';
                     document.getElementById("kar"+id+"_6").hidden = true;
                     document.getElementById("kar"+id+"_5").hidden = true;
                     document.getElementById("kar"+id+"_31").hidden = true;
                     document.getElementById("kar"+id+"_32").hidden = true;
                     document.getElementById("kar"+id+"_41").hidden = true;
                     document.getElementById("kar"+id+"_42").hidden = true;
+                    document.getElementById("kar"+id+"2_6").hidden = true;
+                    document.getElementById("kar"+id+"2_5").hidden = true;
+                    document.getElementById("kar"+id+"2_31").hidden = true;
+                    document.getElementById("kar"+id+"2_32").hidden = true;
+                    document.getElementById("kar"+id+"2_41").hidden = true;
+                    document.getElementById("kar"+id+"2_42").hidden = true;
               }
               if (id[1] == "4" || id[1] == "5" || id[1] == "6" || id[1] == "7" || id[1] == "8"){
                     document.getElementById('t'+id+"kar1").name = 0;
@@ -127,6 +151,30 @@ function exist(id_) {
                     document.getElementById("kar"+id+"_62").hidden = true;
                     document.getElementById("kar"+id+"_21").hidden = true;
                     document.getElementById("kar"+id+"_22").hidden = true;
+                    document.getElementById('t'+id+"2kar1").name = 0;
+                    document.getElementById('t'+id+"2kar5").name = 0;
+                    document.getElementById('t'+id+"2kar61").name = 0;
+                    document.getElementById('t'+id+"2kar62").name = 0;
+                    document.getElementById('t'+id+"2kar21").name = 0;
+                    document.getElementById('t'+id+"2kar22").name = 0;
+                    document.getElementById('t'+id+"2kar1").hidden = true;
+                    document.getElementById('t'+id+"2kar5").hidden = true;
+                    document.getElementById('t'+id+"2kar61").hidden = true;
+                    document.getElementById('t'+id+"2kar62").hidden = true;
+                    document.getElementById('t'+id+"2kar21").hidden = true;
+                    document.getElementById('t'+id+"2kar22").hidden = true;
+                    document.getElementById("kar"+id+"2_1").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_5").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_61").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_62").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_21").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_22").style.backgroundColor = '#f0f0f0';
+                    document.getElementById("kar"+id+"2_1").hidden = true;
+                    document.getElementById("kar"+id+"2_5").hidden = true;
+                    document.getElementById("kar"+id+"2_61").hidden = true;
+                    document.getElementById("kar"+id+"2_62").hidden = true;
+                    document.getElementById("kar"+id+"2_21").hidden = true;
+                    document.getElementById("kar"+id+"2_22").hidden = true;
               }
            }
            else {
@@ -166,6 +214,12 @@ function exist(id_) {
                     document.getElementById("kar"+id+"_32").hidden = false;
                     document.getElementById("kar"+id+"_41").hidden = false;
                     document.getElementById("kar"+id+"_42").hidden = false;
+                    document.getElementById("kar"+id+"2_6").hidden = false;
+                    document.getElementById("kar"+id+"2_5").hidden = false;
+                    document.getElementById("kar"+id+"2_31").hidden = false;
+                    document.getElementById("kar"+id+"2_32").hidden = false;
+                    document.getElementById("kar"+id+"2_41").hidden = false;
+                    document.getElementById("kar"+id+"2_42").hidden = false;
               }
               if (id[1] == "4" || id[1] == "5" || id[1] == "6" || id[1] == "7" || id[1] == "8"){
                     document.getElementById("kar"+id+"_1").hidden = false;
@@ -174,6 +228,12 @@ function exist(id_) {
                     document.getElementById("kar"+id+"_62").hidden = false;
                     document.getElementById("kar"+id+"_21").hidden = false;
                     document.getElementById("kar"+id+"_22").hidden = false;
+                    document.getElementById("kar"+id+"2_1").hidden = false;
+                    document.getElementById("kar"+id+"2_5").hidden = false;
+                    document.getElementById("kar"+id+"2_61").hidden = false;
+                    document.getElementById("kar"+id+"2_62").hidden = false;
+                    document.getElementById("kar"+id+"2_21").hidden = false;
+                    document.getElementById("kar"+id+"2_22").hidden = false;
               }
            }
            pi();
